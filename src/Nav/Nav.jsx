2f16f20fc736221bd605ca46b2bbafa6964b9da7
Ablanya Charles feranmi <charlesablanya@gmail.com>
2025-07-11 17:02:45 +0100

    Youe4

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -192,7 +192,7 @@ const Nav = () => {
 
                <li>
                 <Link className="Link" to="/Reign%20Bites">
-                 Reginbite
+                 Regin Bite
                 </Link>
                 </li>
 
